7b95487757974fa207e3eae7d4da0e249100cd68
nglncbl <ascubelo@up.edu.ph>
2025-05-03 18:06:15 +0800

    feat: implement authentication

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,9 +1,16 @@
-import { register } from "../controllers/register.js";
+
+import { register, removeUser, login, getAllUsers } from "../controllers/userController.js";
 import express from "express";
 
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
+
+router.delete("/removeUser", removeUser);
+
+router.get("/getAllUsers", getAllUsers);
 
 export default router;
