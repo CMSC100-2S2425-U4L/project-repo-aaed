e8bb2041852701ddab0865006af967d2821ff640
djeanabriones <dmbriones1@up.edu.ph>
2025-05-12 19:13:47 +0800

    ui: changed the icons and imported icon library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
 import logo from './assets/logo.png';
+import { FaUserCircle, FaShoppingCart } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="app-container">
       <header className="navbar">
-        <img src={logo} alt="AgriMart Logo" className="logo" />
+        <img src={logo} alt="AgriMart Logo" className="logo"/>
         <nav>
           <ul className="nav-links">
             <li className="active">Home</li>
             <li>Shop</li>
             <li>Cart</li>
             <li>About</li>
-            <li><i className="fas fa-user-circle"></i></li>
+            <li><FaUserCircle size={45}/></li>
           </ul>
         </nav>
       </header>
@@ -21,7 +22,8 @@ function App() {
         <div className="hero-content">
           <img src={logo} alt="AgriMart Main" className="main-logo" />
           <button className="shop-button">
-            🛒 Start Shopping
+            <FaShoppingCart className="shop-icon" />
+            <span>  Start Shopping</span>
           </button>
         </div>
       </main>
