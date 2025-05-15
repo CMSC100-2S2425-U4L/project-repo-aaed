30b5edad586491bc211918c8127e3f2c77c84ae0
AJTJ09 <atjavier1@up.edu.ph>
2025-05-15 10:47:35 +0800

    chore: added more test cases

--- server/test.js
+++ server/test.js
@@ -1,7 +1,28 @@
 import needle from 'needle';
 
 needle.post(
-    'http://localhost:3000/auth/register',
+    'http://localhost:3001/product/add',
+    {
+        productName: "Ewan",
+        productPrice: 100,
+        productDescription: "Test",
+        productType: 2,
+        productQuantity: 10,
+        productImage: "test.jpg"
+
+    },
+    { json: true }, // Ensure the request is sent as JSON
+    (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res.body);
+        }
+    }
+);
+
+needle.post(
+    'http://localhost:3001/auth/register',
     {
         firstName: "Adrian Jericho",
         middleName: "Tagel",
