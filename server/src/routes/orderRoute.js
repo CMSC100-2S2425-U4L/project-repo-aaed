c909c00aece6731240244b91a25d3272986c3c19
djeanabriones <dmbriones1@up.edu.ph>
2025-05-25 11:53:10 +0800

    chore: fixed merge problems in orderRoute

--- server/src/routes/orderRoute.js
+++ server/src/routes/orderRoute.js
@@ -1,33 +1,31 @@
-<<<<<<< HEAD
-// src/routes/orderRoute.js
-import express from 'express';
-import Order from '../models/Order.js'; // Use .js if using ES modules
+// // src/routes/orderRoute.js
+// import express from 'express';
+// import Order from '../models/Order.js'; // Use .js if using ES modules
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/', async (req, res) => {
-  console.log('Received order:', req.body);
-  try {
-    const newOrder = new Order(req.body);
-    await newOrder.save();
-    res.status(201).json(newOrder);
-  } catch (err) {
-    console.error("Order saving failed:", err); // Log the actual error
-    res.status(500).json({ message: 'Failed to save order', error: err.message });
-    }
+// router.post('/', async (req, res) => {
+//   console.log('Received order:', req.body);
+//   try {
+//     const newOrder = new Order(req.body);
+//     await newOrder.save();
+//     res.status(201).json(newOrder);
+//   } catch (err) {
+//     console.error("Order saving failed:", err); // Log the actual error
+//     res.status(500).json({ message: 'Failed to save order', error: err.message });
+//     }
 
-});
+// });
 
-export default router;
-=======
-import {
-    saveOrder,
-    getOrder,
-    getOrdersByEmail,
-    updateOrderStatus,
-    getAllOrders
-} 
-from '../controllers/orderController.js';
+// export default router;
+// import {
+//     saveOrder,
+//     getOrder,
+//     getOrdersByEmail,
+//     updateOrderStatus,
+//     getAllOrders
+// } 
+// from '../controllers/orderController.js';
 import express from 'express';
 
 const router = express.Router();
@@ -49,4 +47,3 @@ router.get("/", (req, res, next) => {
 router.put("/status/:id", updateOrderStatus);
 
 export default router;
->>>>>>> b0b838b8288e633b7851c3a836f419deec335edc
